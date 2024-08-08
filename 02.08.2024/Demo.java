d78a8876fa3bd2af597555978a89bfcd0aecb011
Biswajit Thakur <63996316+BiswajitThakur@users.noreply.github.com>
2024-08-08 21:30:34 +0530

    Update Demo.java

--- 02.08.2024/Demo.java
+++ 02.08.2024/Demo.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class Demo {
   public static void main(String[] args) {
 
-    Department cseDept = new Department("CSE", "KC");
-    cseDept.addStudent(12, "Asikul Ali", 9.5d, 2024);
-    cseDept.addStudent(99, "Sumit Gupta", 9.1d, 2024);
-    cseDept.addStudent(22, "Susmita Barua", 10.0d, 2025);
-    
+    Department cseDept = new Department("CSE", "KC"); // new department created 
+    cseDept.addStudent(12, "Asikul Ali", 9.5d, 2024); // student added, now capacity=3, currentStudent=1
+    cseDept.addStudent(99, "Sumit Gupta", 9.1d, 2024); // student added, now capacity=3, currentStudent=2
+    cseDept.addStudent(22, "Susmita Barua", 10.0d, 2025); // student added, now capacity=3, currentStudent=3
+    // now you can not add any student to cseDept
+    // because, the current status is, capacity=3, currentStudent=3
+    // if you want to add more students update capacity variable present in Department class
 
     Department itDept = new Department("IT", "ABC");
     itDept.addStudent(3, "Foo", 8.9d, 2023);
@@ -19,8 +21,8 @@ public class Demo {
     ctDept.addStudent(41, "Rahul", 9.3d, 2023);
     ctDept.addStudent(55, "Nancy", 9.0d, 2023);
 
-    Department[] dp = new Department[3];
-    dp[0] = cseDept;
+    Department[] dp = new Department[3]; // new array created to store Department
+    dp[0] = cseDept; // assignment cseDept at index 0 to the array
     dp[1] = itDept;
     dp[2] = ctDept;
 
